636da104e8a3328bc5d4ab25d66436583b90a7c0
Temitope Balogun <tsbalogun97@yahoo.com>
2023-08-10 03:10:03 -0500

    done

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
+// function for each key sounds to play onclick
 
-// function for each key sounds
-const letter = document.querySelector('.letter')
-  console.log(letter);
-  playSound()
+const letters = document.querySelectorAll(".key");
+console.log(letters);
 
+let testAudio = document.querySelector("audio");
+document.addEventListener("keydown", (e) => {
+  console.log(e);
+  if (e.keyCode == 65) {
+    let audio = document.querySelector("#clap");
+    audio.play();
+  } else if (e.keyCode == 83) {
+    let audio = document.querySelector("#hihat");
+    audio.play();
+  } else if (e.keyCode == 68) {
+    let audio = document.querySelector("#Kick");
+    audio.play();
+  } else if (e.keyCode == 68) {
+    let audio = document.querySelector("#Kick");
+    audio.play();
+  } else if (e.keyCode == 68) {
+    let audio = document.querySelector("#Kick");
+    audio.play();
+  } else if (e.keyCode == 70) {
+    let audio = document.querySelector("#openhat");
+    audio.play();
+  } else if (e.keyCode == 71) {
+    let audio = document.querySelector("#boom");
+    audio.play();
+  } else if (e.keyCode == 72) {
+    let audio = document.querySelector("#ride");
+    audio.play();
+  } else if (e.keyCode == 74) {
+    let audio = document.querySelector("#snare");
+    audio.play();
+  } else if (e.keyCode == 75) {
+    let audio = document.querySelector("#tom");
+    audio.play();
+  } else if (e.keyCode == 76) {
+    let audio = document.querySelector("#tink");
+    audio.play();
+  }
+});
 
+// testAudio.play()
